d710d287d2eadd54870f19bd1d19f323f4df2155
rajacsp <raja.csp@gmail.com>
2019-02-02 14:05:30 -0500

    Get cities/states dummy added

---
+++ org/tact/random_util.py
@@ -0,0 +1,16 @@
+
+
+def get_cities():
+    return ["chennai", "bangalore"]
+
+def get_states():
+    return  ["Tamilnadu", "Ontario"]
+
+if __name__  == "__main__":
+    #print('test')
+    
+    cities = get_cities()
+    print(cities)
+
+    states = get_states()
+    print(states)
